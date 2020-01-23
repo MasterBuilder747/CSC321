8d938df8c430507344f8107f819c112ef364eb0e
Joseph <jaathunder@yahoo.com>
2020-01-23 11:06:11 -0800

    Initial commit

---
+++ src/Week01/BubbleSort.java
@@ -0,0 +1,24 @@
+package Week01;
+
+public class BubbleSort {
+
+
+    public static void main (String[] args) {
+
+        public static int[] bubblesort(int[] inlist) {
+            
+        }
+
+        int[] sorted = {-2, 3, 7, 0, 4, 3, -1, 8, 5, 5, 5};
+        for (int i : sorted) {
+            System.out.print(i + ", ");
+        }
+        System.out.println();
+        sorted = bubblesort(sorted);
+        for (int i : sorted) {
+            System.out.print(i + ", ");
+        }
+        System.out.println();
+    }
+
+}
