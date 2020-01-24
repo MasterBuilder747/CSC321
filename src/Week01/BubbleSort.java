8139404b5df7920cd84a2ba40be569e0b1b84fd0
Joseph <jaathunder@yahoo.com>
2020-01-24 08:47:24 -0800

    HW1 update

--- src/Week01/BubbleSort.java
+++ src/Week01/BubbleSort.java
@@ -2,12 +2,20 @@ package Week01;
 
 public class BubbleSort {
 
+    public static int[] bubblesort(int[] inlist) {
+
+        int[] out = new int[inlist.length];
+
+        for (int i = 0; i < inlist.length; i++) {
+
+            
+
+        }
+
+        return out;
+    }
 
     public static void main (String[] args) {
-
-        public static int[] bubblesort(int[] inlist) {
-            
-        }
 
         int[] sorted = {-2, 3, 7, 0, 4, 3, -1, 8, 5, 5, 5};
         for (int i : sorted) {
