a3e4285154e3c53711daea88374db4e35f83cc73
Joseph <jaathunder@yahoo.com>
2020-02-29 17:23:55 -0800

    some testing

--- src/Homework/HW8.java
+++ src/Homework/HW8.java
@@ -2,9 +2,27 @@ package Homework;
 
 public class HW8 {
 
+    public static boolean prime(int a) {
+        if (a < 2) {
+            return false;
+        } else {
+            double b = a;
+            for (int i = 0; i < a; i++) {
+                //a / (b + i)
+            }
+        }
+        return true;
+    }
+
     //prime number implementation
     public static void main(String[] args) {
+        System.out.println(prime(1));
 
+        int a = 3;
+        int b = 1;
+        double r = a / (b * 1.0);
+        double r2 = r - (int)r;
+        System.out.println(r2);
     }
 
 }
