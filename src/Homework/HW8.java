87eeebf0f3fce328d5b2d7b30038b9024d68abf3
Joseph <jaathunder@yahoo.com>
2020-03-02 11:55:31 -0800

    add prime number implementation for hw8

--- src/Homework/HW8.java
+++ src/Homework/HW8.java
@@ -2,27 +2,26 @@ package Homework;
 
 public class HW8 {
 
-    public static boolean prime(int a) {
-        if (a < 2) {
+    //prime number implementation
+    public static boolean prime(int n) {
+        if (n < 2) {
             return false;
-        } else {
-            double b = a;
-            for (int i = 0; i < a; i++) {
-                //a / (b + i)
+        }
+        for (int i = 2; i < n; i++) {
+            //if there is any exact division (aka remainder == 0)
+            //of any numbers starting with 2 until right before n,
+            //it is not a prime number
+            //otherwise, it is
+            if (n % i == 0) { //somehow modulo needs to be implemented in assembly
+                return false;
             }
         }
         return true;
     }
 
-    //prime number implementation
     public static void main(String[] args) {
-        System.out.println(prime(1));
-
-        int a = 3;
-        int b = 1;
-        double r = a / (b * 1.0);
-        double r2 = r - (int)r;
-        System.out.println(r2);
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i + ": " + prime(i));
+        }
     }
-
 }
